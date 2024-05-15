8a3ead6bb26b028b2f5c1d7301956643e3fba0b7
TEJASVI_B <167291410+TejasviBhadavankar@users.noreply.github.com>
2024-05-15 19:46:06 +0530

    Update landingpage.js

--- landingpage.js
+++ landingpage.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', () => {
   const nav = document.querySelector('nav');
   if (window.scrollY > 100) {
-      nav.style.backgroundColor = '#BB8493'; // Tomato
+      nav.style.backgroundColor = '#BB8493'; 
   } else {
       nav.style.backgroundColor = '#333'; // Original color
   }
